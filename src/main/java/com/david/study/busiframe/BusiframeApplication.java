89ef6530a6f5197140d771882e3412999c716821
davidyang <qsssyang@163.com>
2020-04-03 15:36:56 +0800

    init

---
+++ src/main/java/com/david/study/busiframe/BusiframeApplication.java
@@ -0,0 +1,13 @@
+package com.david.study.busiframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusiframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusiframeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/david/study/busiframe/BusiframeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.study.busiframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusiframeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
